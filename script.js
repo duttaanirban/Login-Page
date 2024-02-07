6b64a4b2fbe8d13d2818405b3170e329646091a1
duttaanirban <duttaanirbanad@gmail.com>
2024-02-07 19:22:38 +0530

    updated

---
+++ script.js
@@ -0,0 +1,14 @@
+function validateForm() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    if (username === "" || password === "") {
+        alert("Please enter both username and password.");
+    } else {
+        alert("Form submitted successfully");
+    }
+    document.getElementById("username").value="";
+    document.getElementById("password").value="";
+
+
+}
